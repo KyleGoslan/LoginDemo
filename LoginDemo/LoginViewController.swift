08b58d5fa2589c66ead1437195b3a95795f69fe4
Kyle Goslan <KyleGoslan@users.noreply.github.com>
2018-02-06 13:43:56 +0000

    Setup

---
+++ LoginDemo/LoginViewController.swift
@@ -0,0 +1,12 @@
+import UIKit
+
+class LoginViewController: UIViewController {
+
+  @IBOutlet weak var emailTextField: UITextField!
+  @IBOutlet weak var passwordTextField: UITextField!
+  
+  @IBAction func login(_ sender: Any) {
+  
+  }
+  
+}
---
+++ Podfile
@@ -0,0 +1,12 @@
+platform :ios, '11.0'
+
+target 'LoginDemo' do
+
+  use_frameworks!
+
+  pod 'Firebase/Core'
+	pod 'FirebaseInstanceID'
+	pod 'Firebase/Database'
+	pod 'Firebase/Auth'
+
+end
